b36f0532779480bcfa8099a2fcf1b90b2a6a1c30
Ewan Donnachie <ewan@donnachie.net>
2021-04-09 21:00:02 +0000

    version 1.2.2

---
+++ inst/doc/coding_covid-19.R
@@ -0,0 +1,10 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, eval = FALSE, include = FALSE-------------------------------------
+#  library(ICD10gm)
+#  library(dplyr)
+
